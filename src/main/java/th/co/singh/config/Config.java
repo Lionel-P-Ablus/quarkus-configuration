4a7f9f9df3de649bc74f3e3497dbf38662f0bae0
Praneed.K <>
2021-02-05 20:30:05 +0700

    [example] how to create our own config and get config value

--- src/main/java/th/co/singh/config/Config.java
+++ src/main/java/th/co/singh/config/Config.java
@@ -1,4 +1,31 @@
 package th.co.singh.config;
 
+import io.quarkus.arc.config.ConfigProperties;
+import io.quarkus.runtime.annotations.ConfigItem;
+
+@ConfigProperties(prefix = "config") // replace your prefix name here
 public class Config {
+    @ConfigItem(
+            name = "server-address", // replace your item name here
+            defaultValue = "12.111.11.111" // replace your defaultValue here
+    )
+    public String serverAddress;
+
+    @ConfigItem(
+            name = "port",
+            defaultValue = "22"
+    )
+    public String port;
+
+    @ConfigItem(
+            name = "user-name",
+            defaultValue = "user"
+    )
+    public String userName;
+
+    @ConfigItem(
+            name = "password",
+            defaultValue = "1234"
+    )
+    public String password;
 }
--- src/main/java/th/co/singh/config/Resource.java
+++ src/main/java/th/co/singh/config/Resource.java
@@ -1,16 +1,28 @@
 package th.co.singh.config;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@Slf4j
 @Path("/api")
 public class Resource {
+
+    @Inject
+    Config config;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        log.info("server address: " + config.serverAddress);
+        log.info("server port: " + config.port);
+        log.info("server user name: " + config.userName);
+        log.info("server password: " + config.password);
+
+        return "Example get value from config work!!!";
     }
 }
