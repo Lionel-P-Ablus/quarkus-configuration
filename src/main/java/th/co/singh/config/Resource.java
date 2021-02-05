1f2358cca60c06c26120f76f52862ac4c09293b8
Praneed.K <>
2021-02-05 20:43:45 +0700

    [update]

--- src/main/java/th/co/singh/config/Resource.java
+++ src/main/java/th/co/singh/config/Resource.java
@@ -12,12 +12,15 @@ import javax.ws.rs.core.MediaType;
 @Path("/api")
 public class Resource {
 
-    @Inject
+    @Inject // Inject config class that we wanna use a value
     Config config;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        // we can get a value following the code below
+        // config meaning name of Config class that we inject in
+        // serverAddress meaning name of variables that we specific
         log.info("server address: " + config.serverAddress);
         log.info("server port: " + config.port);
         log.info("server user name: " + config.userName);
